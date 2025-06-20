b9dd538d52a100b55c2ee92d4d6f27a8e626765b
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-06-20 18:53:11 +0600

    working on book model

---
+++ src/models/book.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IBook extends mongoose.Document {
+  title: string;
+  author: string;
+  genre:
+    | "FICTION"
+    | "NON_FICTION"
+    | "SCIENCE"
+    | "HISTORY"
+    | "BIOGRAPHY"
+    | "FANTASY";
+  isbn: string;
+  description: string;
+  copies: number;
+  available: boolean;
+}
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "FICTION",
+      "NON_FICTION",
+      "SCIENCE",
+      "HISTORY",
+      "BIOGRAPHY",
+      "FANTASY",
+    ],
+  },
+  isbn: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  copies: { type: Number, required: true, min: 0 },
+  available: { type: Boolean, required: true, default: true },
+});
+
+export const Book = mongoose.model<IBook>("Book", bookSchema);
+export default Book;
+export type { IBook };
