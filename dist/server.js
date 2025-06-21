49338b874a44ba446835f0364c16acab03b3be11
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-06-21 21:11:52 +0600

    imported dotenv

--- dist/server.js
+++ dist/server.js
@@ -4,5 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const port = process.env.PORT || 5000;
 app_1.default.listen(port, () => console.log(`Server running on http://localhost:${port}`));
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
+import dotenv from "dotenv";
 
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
