f92d09ed0338dfab7b690406e6bad1f1e39a2126
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-06-21 23:36:29 +0600

    dbConnection function declaration changed from async to sync

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,13 +7,15 @@ exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield mongoose_1.default.connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/playground");
+const connectDB = () => {
+    mongoose_1.default
+        .connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/playground")
+        .then(() => {
         console.log("Connected to MongoDB");
-    }
-    catch (error) {
+    })
+        .catch((error) => {
         console.error("Error connecting to MongoDB:", error);
-    }
-});
+    });
+};
 exports.connectDB = connectDB;
+exports.default = exports.connectDB;
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/playground"
-    );
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/playground")
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
 };
+
+export default connectDB;
